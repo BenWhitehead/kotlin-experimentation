778a0ba7665c09b569fd1640e0cdc9e2fa77edf3
BenWhitehead <benjamin.r.whitehead@gmail.com>
2012-08-06 12:03:13 -0600

    Added a small program to print out all system properties

---
+++ src/com/github/benwhitehead/kotlin/fromjava/PrintSystemProperties.kt
@@ -0,0 +1,36 @@
+package com.github.benwhitehead.kotlin.fromjava
+
+import java.util.Map
+import java.util.Properties
+import java.util.TreeMap
+
+/**
+* @author Ben Whitehead
+*/
+public open class PrintSystemProperties() {
+    class object {
+        public open fun main(args : Array<String>) : Unit {
+            val properties : Properties? = System.getProperties()
+            val props : Map<String?, String?>? = TreeMap()
+            for (entry in properties?.entrySet())
+            {
+                var key : String? = ""
+                var value : String? = ""
+                if ((entry?.getKey() is String?))
+                {
+                    key = (entry?.getKey() as String?)
+                }
+                if ((entry?.getValue() is String?))
+                {
+                    value = (entry?.getValue() as String?)
+                }
+                props?.put(key, value)
+            }
+            for (entry : Map.Entry<String?, String?>? in props?.entrySet())
+            {
+                System.out.println(entry?.getKey() + " => " + entry?.getValue())
+            }
+        }
+    }
+}
+fun main(args : Array<String>) = PrintSystemProperties.main(args)
